4ea504b9a1fbc2635cb3657981d75cd514c2d181
Bryson <rikbrysn@gmail.com>
2026-03-09 08:18:09 +0800

    Updated Icon and Images [March 9, 2026]

--- ToDoList/obj/Debug/net10.0-ios/iossimulator-x64/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net10.0-ios/iossimulator-x64/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ae379b48984bac53b46c36af0664e014c8f1dc2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3c529f80e02bf17fc829ff720af1434ac87b19fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
