1a621d7c22aa2f2beb89da08ce545e2e089d7e76
Bryson <rikbrysn@gmail.com>
2026-04-23 07:00:18 +0800

    Update [04/23/2026]
    Fixed app causing error when running

--- ToDoList/obj/Debug/net10.0-android/android-x64/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net10.0-android/android-x64/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+be514bcbe2e032abad084e8851e5db95f5868c01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3d7fec7c5d7ed50f6f4ffd6eb838c853c9f7a357")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
